47f5bfefccf1c30071da13b653dd20c8c4b9d02d
ikura <ikura554@gmail.com>
2016-11-29 03:50:26 +0000

    Update Gemfile.lock for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'blogs#index'
+  resources :blogs do
+  resources :replies
+end
+
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
