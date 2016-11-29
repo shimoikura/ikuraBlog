64bc3457ed8248c318f7bd2485acebaf20d08f7a
ikura <ikura554@gmail.com>
2016-11-29 11:06:17 +0000

    like

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,16 +12,18 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
     @blog.save
   end
+  def increment
+    @blog = Blog.find(params[:id])
+    @blog.like += 1
+    @blog.save
+    redirect_to :back
+  end
   private
      def blog_params
        params.require(:blog).permit(:body)
      end
 
-  def increment
-    @blog = Blog.find(params[:id])
-    @blog.like += 1
-    @blog.save
-  end
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :blogs do
   resources :replies
 end
-get 'increment.json',to: 'blogs#increment'
+post '/blogs/:id/increment', to: 'blogs#increment', as: 'like'
 
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
